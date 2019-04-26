4460113bfa2aad058e97cb3b0d9717bf3de85898
Audra Tallman <audi.tallman@gmail.com>
2019-04-26 15:27:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,21 +3,29 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/reversename/:name'
-    @name = params[:name].reverse 
+    @reversename = params[:name].reverse 
   end 
   
   get '/square/:number'
+    @square = params[:number] * params[:number]
+    "#{@square}"
   end   
 
-
-
   get '/say/:number/:phrase'
+    @say = params[:phrase].times params[:number]
+    "#{@say}"
   end 
 
   get '/say/:word1/:word2/:word3/:word4/:word5'
+    @say = params[:word1/:word2/:word3/:word4/:word5]
+    "#{@say}"
   end 
 
-
+  get '/:operation/:number1/:number2'
+  if params[:operation] == add 
+    @sum = params[:number1] + params[:number2]
+    
+  end 
 
 
 end
