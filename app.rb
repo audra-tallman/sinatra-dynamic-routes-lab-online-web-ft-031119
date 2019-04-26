9b61b1a5c3147c21a552f6da5bf6e672027aa595
Audra Tallman <audi.tallman@gmail.com>
2019-04-26 15:41:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,14 +20,16 @@ class App < Sinatra::Base
   end 
 
   get '/say/:word1/:word2/:word3/:word4/:word5'
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]} 
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
   end 
 
   get '/:operation/:number1/:number2'
-  if params[:operation] == add 
-    @sum = params[:number1] + params[:number2]
-    "#{@sum}"
-  elsif params[:operation]  == subtract
+    op = params[:operation]
+    n1 = params[:number1].to_i
+    n2 = params[:number2].to_i
+  if op == "add"
+    sum = n1 + n2
+  elsif op  == "subtract"
     @difference = params[:number1] - params[:number2]
     "#{@difference}"
   elsif params[:operation]  == multiply
